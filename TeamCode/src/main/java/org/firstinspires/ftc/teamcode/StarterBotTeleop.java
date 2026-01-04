c5ef9159ac0f67f0ec9bba0e3dba84ed71181a80
Sai Mukunthan <saimukunthans@gmail.com>
2026-01-04 16:04:14 -0500

    Changed speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleop.java
@@ -71,7 +71,7 @@ public class StarterBotTeleop extends OpMode {
      * velocity. Here we are setting the target, and minimum velocity that the launcher should run
      * at. The minimum velocity is a threshold for determining when to fire.
      */
-    final double LAUNCHER_TARGET_VELOCITY = 1300 ;
+    final double LAUNCHER_TARGET_VELOCITY = 1301 ;
     final double LAUNCHER_MIN_VELOCITY = 1200;
 
     // Declare OpMode members.
